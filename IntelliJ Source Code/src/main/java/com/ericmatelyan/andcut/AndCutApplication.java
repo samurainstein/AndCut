793978b7965208393532766aed184fba677fc565
samurainstein <eric.matelyan@gmail.com>
2022-09-05 15:06:08 -0400

    Initialization of repository

---
+++ IntelliJ Source Code/src/main/java/com/ericmatelyan/andcut/AndCutApplication.java
@@ -0,0 +1,13 @@
+package com.ericmatelyan.andcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndCutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndCutApplication.class, args);
+	}
+
+}
