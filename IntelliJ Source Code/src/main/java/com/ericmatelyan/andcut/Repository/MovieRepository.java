d98c77d6b6c5b34c1a931047bbd9efde3e1978ec
samurainstein <eric.matelyan@gmail.com>
2022-09-05 16:31:56 -0400

    Create MovieRepository Interface

---
+++ IntelliJ Source Code/src/main/java/com/ericmatelyan/andcut/Repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.ericmatelyan.andcut.Repository;
+
+import com.ericmatelyan.andcut.Model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    
+}
